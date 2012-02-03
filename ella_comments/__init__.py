5eec162da560edca207d8d6ca942379a702f4ceb
Jakub Vysoky <jakub@borka.cz>
2012-02-03 20:51:17 +0100

    plain comment form for authorized only sites

--- ella_comments/__init__.py
+++ ella_comments/__init__.py
@@ -2,12 +2,18 @@
 Change the attributes you want to customize
 """
 
+from django.conf import settings
+
 from threadedcomments.models import ThreadedComment
 from threadedcomments.forms import ThreadedCommentForm
+
+from ella_comments.forms import AuthorizedCommentForm
 
 def get_model():
     return ThreadedComment
 
 def get_form():
+    if getattr(settings, 'COMMENTS_AUTHORIZED_ONLY', False):
+        return AuthorizedCommentForm
     return ThreadedCommentForm
 
---
+++ ella_comments/forms.py
@@ -0,0 +1,29 @@
+import datetime
+
+from django.contrib.contenttypes.models import ContentType
+from django.utils.encoding import force_unicode
+from django.conf import settings
+
+from threadedcomments.forms import ThreadedCommentForm
+
+
+class AuthorizedCommentForm(ThreadedCommentForm):
+    def __init__(self, *args, **kwargs):
+        "there is no such thing as user_name, user_email, user_url"
+        super(AuthorizedCommentForm, self).__init__(*args, **kwargs)
+        self.fields.pop('name')
+        self.fields.pop('email')
+        self.fields.pop('url')
+
+    def get_comment_create_data(self):
+        "so remove it from comment create date"
+        return dict(
+            content_type = ContentType.objects.get_for_model(self.target_object),
+            object_pk    = force_unicode(self.target_object._get_pk_val()),
+            comment      = self.cleaned_data["comment"],
+            submit_date  = datetime.datetime.now(),
+            site_id      = settings.SITE_ID,
+            is_public    = True,
+            is_removed   = False,
+        )
+
