cbc27a1a28683435374e7691d2d3c666fb1a9544
Honza Kral <honza.kral@gmail.com>
2012-03-19 20:45:08 +0100

    reflect comment moderation

--- ella_comments/listing_handlers.py
+++ ella_comments/listing_handlers.py
@@ -2,6 +2,7 @@ import logging
 import time
 
 from django.db.models.loading import get_model
+from django.contrib import comments
 
 from ella.core.cache.redis import RedisListingHandler, client
 from ella.core.models import Publishable
@@ -28,6 +29,50 @@ class MostCommentedListingHandler(RedisListingHandler):
 
 class LastCommentedListingHandler(RedisListingHandler):
     PREFIX = 'lastcom'
+
+def comment_pre_save(instance, **kwargs):
+    if instance.pk:
+        old_instance = instance.__class__._default_manager.get(pk=instance.pk)
+        instance.__pub_info = {
+            'is_public': old_instance.is_public,
+            'is_removed': old_instance.is_removed,
+        }
+
+def comment_post_save(instance, **kwargs):
+    if hasattr(instance, '__pub_info'):
+        is_public = instance.is_public and not instance.is_removed
+        was_public = instance.__pub_info['is_public'] and not instance.__pub_info['is_removed']
+
+        # comment being moderated
+        if was_public and not is_public:
+            count_key = COMCOUNT_KEY % (instance.content_type_id, instance.object_pk)
+            client.decr(count_key)
+
+        # commet back up
+        elif not was_public and is_public:
+            count_key = COMCOUNT_KEY % (instance.content_type_id, instance.object_pk)
+            client.incr(count_key)
+        else:
+            # no change
+            return
+
+        last_keu = LASTCOM_KEY % (instance.content_type_id, instance.object_pk)
+        try:
+            # update the last comment info
+            last_com = comments.get_model()._default_manager.filter(content_type_id=instance.content_type_id, object_pk=instance.object_pk, is_public=True, is_removed=False).latest('submit_date')
+            client.hmset(last_keu, {
+                'submit_date': repr(time.mktime(last_com.submit_date.timetuple())),
+                'user_id': last_com.user_id or '',
+                'username': last_com.user.username if last_com.user_id else last_com.user_name,
+            })
+        except comments.get_model().DoesNotExist:
+            client.delete(last_keu)
+
+        # update the listing handlers
+        obj = instance.content_object
+        if isinstance(obj, Publishable) and obj.is_published():
+            publishable_published(obj)
+
 
 def publishable_unpublished(publishable, **kwargs):
     pipe = client.pipeline()
@@ -64,15 +109,19 @@ def comment_posted(comment, **kwargs):
 
     obj = comment.content_object
     if isinstance(obj, Publishable) and obj.is_published():
-        publishable_published(obj, last_comment=comment)
+        publishable_published(obj)
 
 def connect_signals():
     from django.contrib.comments.signals import comment_was_posted
     from ella.core.signals import content_published, content_unpublished
+    from django.db.models.signals import pre_save, post_save
     content_published.connect(publishable_published)
     content_unpublished.connect(publishable_unpublished)
 
     comment_was_posted.connect(comment_posted)
+
+    pre_save.connect(comment_pre_save, sender=comments.get_model())
+    post_save.connect(comment_post_save, sender=comments.get_model())
 
 if client:
     connect_signals()
