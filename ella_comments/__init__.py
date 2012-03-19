3556831a618badc1c46c94387ea4897983942f4d
Jakub Vysoky <jakub@borka.cz>
2012-03-19 18:15:10 +0100

    enable custom form

--- ella_comments/__init__.py
+++ ella_comments/__init__.py
@@ -3,6 +3,8 @@ Change the attributes you want to customize
 """
 
 from django.conf import settings
+from django.utils.importlib import import_module
+from django.core.exceptions import ImproperlyConfigured
 
 from threadedcomments.models import ThreadedComment
 from threadedcomments.forms import ThreadedCommentForm
@@ -13,6 +15,19 @@ def get_model():
     return ThreadedComment
 
 def get_form():
+    custom_comment_form = getattr(settings, 'COMMENTS_CUSTOM_FORM', None)
+    if custom_comment_form:
+        try:
+            module_name, klass_name = custom_comment_form.rsplit('.', 1)
+            module = import_module(module_name)
+        except ImportError, e:
+            raise ImproperlyConfigured('Error importing custom comment form %s: "%s"' % (klass_name, e))
+        try:
+            CustomCommentForm = getattr(module, klass_name)
+        except AttributeError:
+            raise ImproperlyConfigured('Module "%s" does not define a comment form name "%s"' % (module, klass_name))
+        else:
+            return CustomCommentForm
     if getattr(settings, 'COMMENTS_AUTHORIZED_ONLY', False):
         return AuthorizedCommentForm
     return ThreadedCommentForm
