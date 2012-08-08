93a763cddcba5ff0dceadd7c331d323a4c27655d
Honza Kral <honza.kral@gmail.com>
2012-08-08 14:07:39 +0200

    simler urls

--- ella_comments/urls.py
+++ ella_comments/urls.py
@@ -7,10 +7,10 @@ from ella_comments.views import list_comments, post_comment, update_comment, com
 
 urlpatterns = patterns('',
     url(r'^$', list_comments, name='comments-list'),
+    url(r'^(?P<comment_id>\d+)/$', comment_detail, name='comment-detail'),
     url(r'^%s/(?P<comment_id>\d+)/$' % slugify(_('update')), update_comment, name='comments-update'),
     url(r'^%s/$' % slugify(_('new')), post_comment, name='comments-new'),
     url(r'^%s/(?P<parent_id>\d+)/$' % slugify(_('new')), post_comment, name='comments-reply'),
-    url(r'^detail/(?P<comment_id>[0-9]+)/$', comment_detail, name='comment-detail'),
 )
 
 resolver.register(urlpatterns, prefix=slugify(_('comments')))
